cece7ca88114cf752c3405d8d76b6398d56a49b8
Chyyni <catharina0409@gmail.com>
2024-01-25 16:47:02 +0900

    Chore: 기본 폴더 세팅

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,12 @@
+// 일단 샘플 파일로 지우지 않고 놔두겠습니다.
+
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from "next";
+import type { NextApiRequest, NextApiResponse } from 'next';
 
 type Data = {
   name: string;
 };
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>,
-) {
-  res.status(200).json({ name: "John Doe" });
+export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+  res.status(200).json({ name: 'John Doe' });
 }
