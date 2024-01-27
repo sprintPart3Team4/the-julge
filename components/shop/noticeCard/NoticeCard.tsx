86ca5750203f11ed517b2c74a4f649d6d8bc06af
Jisun Park <jisunpark.t@gmail.com>
2024-01-27 18:13:14 +0900

    Fix: 공고 카드 isClosed 프롭 이름 오타 수정

--- components/shop/noticeCard/NoticeCard.tsx
+++ components/shop/noticeCard/NoticeCard.tsx
@@ -17,18 +17,18 @@ type NoticeCardProps = {
 export default function NoticeCard({ isClosed = false }: NoticeCardProps) {
   return (
     <div className={cn("container", { closed: isClosed })}>
-      {closed && <div className={cn("imgOverlay")}>마감 완료</div>}
+      {isClosed && <div className={cn("imgOverlay")}>마감 완료</div>}
       <Image className={cn("image")} src={TestImage} alt="테스트 이미지" width={280} />
       <div className={cn("contents")}>
         <span className={cn("shopName")}>{storeInfo.item.name}</span>
         <div className={cn("time")}>
-          <Image src={closed ? GreyClockIcon : ClockIcon} alt="시계 아이콘" width={20} />
+          <Image src={isClosed ? GreyClockIcon : ClockIcon} alt="시계 아이콘" width={20} />
           <span>
             {noticeList.items[0].item.startsAt} ({noticeList.items[0].item.workhour}시간)
           </span>
         </div>
         <div className={cn("location")}>
-          <Image src={closed ? GreyLocationIcon : LocationIcon} alt="장소 아이콘" width={20} />
+          <Image src={isClosed ? GreyLocationIcon : LocationIcon} alt="장소 아이콘" width={20} />
           <span>{storeInfo.item.address1}</span>
         </div>
         <span className={cn("pay")}>{noticeList.items[0].item.hourlyPay.toLocaleString()}원</span>
