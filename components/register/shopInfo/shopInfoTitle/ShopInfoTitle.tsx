34f49f16f96ec3c0b51018692449670454d00be0
mijin0928 <94034865+mijin0928@users.noreply.github.com>
2024-01-27 13:28:40 +0900

    Refactor:사장님 - 가게정보등록 타이틀 부분 리팩토링

--- components/register/shopInfo/shopInfoTitle/ShopInfoTitle.tsx
+++ components/register/shopInfo/shopInfoTitle/ShopInfoTitle.tsx
@@ -1,13 +1,12 @@
-import { ReactNode } from "react";
 import classNames from "classnames/bind";
 import styles from "@/components/register/shopInfo/shopInfoTitle/ShopInfoTitle.module.scss";
 
 const cn = classNames.bind(styles);
 
-export default function ShopInfoTitle({ children }: { children: ReactNode }) {
+export default function ShopInfoTitle({ title }: { title: string }) {
   return (
     <div className={cn("container")}>
-      <h2 className={cn("title")}>{children}</h2>
+      <h2 className={cn("title")}>{title}</h2>
       <button className={cn("buttonCancel")} type="button">
         취소
       </button>
--- components/register/shopInfo/shopInfoTitle/Title.tsx
+++
@@ -1,9 +0,0 @@
-import ShopInfoTitle from "@/components/register/shopInfo/shopInfoTitle/ShopInfoTitle";
-
-export default function Title() {
-  return (
-    <>
-      <ShopInfoTitle>가게 정보</ShopInfoTitle>
-    </>
-  );
-}
