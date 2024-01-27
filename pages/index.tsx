4447a16ff2e6afc1ce9a629e756d1f34a6146977
mijin0928 <94034865+mijin0928@users.noreply.github.com>
2024-01-27 21:02:02 +0900

    Design: 사장님/가게정보등록-타이틀 margin-bottom값 수정

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,9 @@
 // 공고 리스트 페이지
-import ShopInfoForm from "@/components/register/shopInfo/shopInfoForm/shopInfoForm";
-import ShopInfoTitle from "@/components/register/shopInfo/shopInfoTitle/ShopInfoTitle";
+
 export default function Home() {
   return (
     <>
       <main>메인페이지</main>
-      <ShopInfoTitle title="가게정보"/>
-      <ShopInfoForm />
     </>
   );
 }
