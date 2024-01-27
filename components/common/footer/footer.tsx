0388d04494e5e3f49b477a5d1df12c28c4844093
Hyunsoo_kim <red_panda119@naver.com>
2024-01-27 15:56:26 +0900

    Feat: 공통/Footer컴포넌트 구현

--- components/common/footer/footer.tsx
+++ components/common/footer/footer.tsx
@@ -8,13 +8,13 @@ const cn = classNames.bind(styles);
 export default function Footer() {
     return (
         <div className={cn("borderBox")}>
-            <div className={cn(styles.contentBox)}>
-                <div className={cn(styles.company, styles.content)}>
+            <div className={cn("contentBox")}>
+                <div className={cn("company", "content")}>
                     <p>©codeit - 2023</p>
                 </div>
-                <div className={cn(styles.customerSupport, styles.content)}>
-                    <p className={cn(styles.privacyPolicy)}>Privacy Policy</p>
-                    <p className={cn(styles.faq)}>FAQ</p>
+                <div className={cn("customerSupport", "content")}>
+                    <p className={cn("privacyPolicy")}>Privacy Policy</p>
+                    <p className={cn("faq")}>FAQ</p>
                 </div>
                 <div className={cn("sns", "content")}>
                     <Link href={"malito:"}><Image src="/images/envelope.svg" width={25} height={25} alt="envelope" /></Link>
