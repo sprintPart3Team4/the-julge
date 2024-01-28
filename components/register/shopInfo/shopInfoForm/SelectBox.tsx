7e4707462d9410d3ecd0b102509c198eec3d5052
mijin0928 <94034865+mijin0928@users.noreply.github.com>
2024-01-28 22:24:40 +0900

    Feat: 사장님/가게정보등록-셀렉트박스기능구현

--- components/register/shopInfo/shopInfoForm/SelectBox.tsx
+++ components/register/shopInfo/shopInfoForm/SelectBox.tsx
@@ -1,3 +1,4 @@
+import { useState, MouseEvent } from "react";
 import { Props } from "@/components/register/shopInfo/shopInfoForm/type";
 import { Item } from "@/components/register/shopInfo/shopInfoForm/type";
 import classNames from "classnames/bind";
@@ -5,18 +6,30 @@ import styles from "@/components/register/shopInfo/shopInfoForm/shopInfoForm.mod
 
 const cn = classNames.bind(styles);
 
-export default function Input({ title, item } : Props) {
+export default function Input({ title, item }: Props) {
   const items = item || [];
+  const [optionValues, setOptionValues] = useState<string>("선택");
+  const [isSelected, setIsSelected] = useState<string>("");
+
+  const handleOpenClick = (e: MouseEvent<HTMLDivElement>) => e.currentTarget.classList.toggle(cn("active"));
+
+  const handleOptionClick = (item: string) => {
+    setOptionValues(item);
+    setIsSelected("active");
+  };
 
   return (
-    <div className={cn("inputBox")}>
+    <div className={cn("inputBox")} onClick={handleOpenClick}>
       <p className={cn("title")}>{title}</p>
-      <label htmlFor="shopName" className={cn("option")}>
-        선택<span className={cn("dropdownOpen")}>열기</span>
+      <label htmlFor="shopName" className={cn("option", isSelected)}>
+        {optionValues}
+        <span className={cn("dropdownOpen")}>열기</span>
       </label>
       <ul>
         {items.map((item: Item) => (
-          <li key={item.id}>{item.name}</li>
+          <li key={item.id} onClick={() => handleOptionClick(item.name)}>
+            {item.name}
+          </li>
         ))}
       </ul>
     </div>
