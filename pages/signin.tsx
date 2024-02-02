2dc46e3799d8065ef8089ed1fa768f957ce0fe8a
Hyunsoo_kim <red_panda119@naver.com>
2024-02-02 12:50:59 +0900

    main Pull

--- pages/signin.tsx
+++ pages/signin.tsx
@@ -1,6 +1,6 @@
 import SigninForm from "@/components/sign/signinForm/SigninForm";
 
-export default function Signup() {
+export default function Signin() {
   return (
     <div>
       <SigninForm />
