44dfa83a9bc211dd826387d7920e07177518a73d
mijin0928 <94034865+mijin0928@users.noreply.github.com>
2024-02-03 20:15:32 +0900

    Feat: x버튼 기능 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,13 @@
 // 공고 리스트 페이지
 
 import Footer from "@/components/common/footer/Footer";
-
+import ShopInfoTitle from "@/components/register/shopInfo/shopInfoTitle/ShopInfoTitle";
 export default function Home() {
   return (
     <>
-      <main>메인페이지</main>
-      <Footer />
+      {/* <main>메인페이지</main> */}
+      {/* <Footer /> */}
+      <ShopInfoTitle title="가게정보"/>
     </>
   );
 }
