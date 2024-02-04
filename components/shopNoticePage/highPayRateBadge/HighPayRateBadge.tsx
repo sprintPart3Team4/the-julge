271ae75bfab1d8280752cb18e606e0e2d21a0750
Jisun Park <jisunpark.t@gmail.com>
2024-02-05 01:40:03 +0900

    Design: 공통-시급인상배지-인상률에 따른 배지 색상

--- components/shopNoticePage/highPayRateBadge/HighPayRateBadge.tsx
+++ components/shopNoticePage/highPayRateBadge/HighPayRateBadge.tsx
@@ -3,8 +3,10 @@ import classNames from "classnames/bind";
 import { useMediaQuery } from "react-responsive";
 
 import ArrowUpWhite from "@/public/images/arrow-up-bold-white.svg";
-import ArrowUpRed from "@/public/images/arrow-up-bold-red.svg";
 import ArrowUpGrey from "@/public/images/arrow-up-bold-grey.svg";
+import ArrowUpRed40 from "@/public/images/arrow-up-bold-red40.svg";
+import ArrowUpRed30 from "@/public/images/arrow-up-bold-red30.svg";
+import ArrowUpRed20 from "@/public/images/arrow-up-bold-red20.svg";
 
 import styles from "./HighPayRateBadge.module.scss";
 
@@ -23,7 +25,19 @@ export default function HighPayRateBadge({
   hourlyPay,
   originalHourlyPay,
 }: Props) {
-  const percentage = ((hourlyPay / originalHourlyPay) * 100).toFixed();
+  const percentage = Number(((hourlyPay / originalHourlyPay) * 100 - 100).toFixed());
+
+  let color, mobileRedArrow;
+  if (percentage < 20) {
+    color = "red20";
+    mobileRedArrow = ArrowUpRed20;
+  } else if (percentage < 50) {
+    color = "red30";
+    mobileRedArrow = ArrowUpRed30;
+  } else {
+    color = "red40";
+    mobileRedArrow = ArrowUpRed40;
+  }
 
   const isMobile: boolean = useMediaQuery({
     query: "(max-width:743px)",
@@ -32,19 +46,21 @@ export default function HighPayRateBadge({
   let arrow = ArrowUpWhite;
 
   if (isMobile && isListedCard) {
-    arrow = closed ? ArrowUpGrey : ArrowUpRed;
+    arrow = closed ? ArrowUpGrey : mobileRedArrow;
   }
 
   return (
-    <div className={cn(isMobile && isListedCard ? "mobileBadge" : "badge", { closed })}>
-      <span className={cn("percentage")}>기존 시급보다 {percentage}%</span>
-      <Image
-        className={cn("icon")}
-        src={arrow}
-        alt="기존 시급보다 높은 시급이라는 걸 표시하기 위해 위를 가리키는 화살표"
-        width={20}
-        height={20}
-      />
-    </div>
+    percentage != 0 && (
+      <div className={cn(isMobile && isListedCard ? "mobileBadge" : "badge", { closed }, color)}>
+        <span className={cn("percentage")}>기존 시급보다 {percentage}%</span>
+        <Image
+          className={cn("icon")}
+          src={arrow}
+          alt="기존 시급보다 높은 시급이라는 걸 표시하기 위해 위를 가리키는 화살표"
+          width={20}
+          height={20}
+        />
+      </div>
+    )
   );
 }
