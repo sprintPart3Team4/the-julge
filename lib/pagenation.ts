e639662f6823cd972226fdc72e1c61736bb5930a
Chyyni <catharina0409@gmail.com>
2024-02-04 17:39:48 +0900

    Feat: 페이지네이션 기능 구현

---
+++ lib/pagenation.ts
@@ -0,0 +1,39 @@
+const getPageGroup = (limitPerPageGroup: number, totalPageNumber: number, pageGroupNumber: number) => {
+  const startNumberOfPageGroup = (pageGroupNumber - 1) * limitPerPageGroup + 1;
+  let lastNumberOfPageGroup = pageGroupNumber * limitPerPageGroup;
+  if (totalPageNumber < lastNumberOfPageGroup) {
+    lastNumberOfPageGroup = totalPageNumber;
+  }
+  return new Array(lastNumberOfPageGroup - startNumberOfPageGroup + 1)
+    .fill(0)
+    .map((_, i) => startNumberOfPageGroup + i);
+};
+
+export const getCurrentPageGroup = (
+  LIMIT_PER_PAGE_GROUP: number,
+  totalPageNumber: number,
+  currentPageNumber: number = 1
+) => {
+  const pageGroupNumber = Math.ceil(currentPageNumber / LIMIT_PER_PAGE_GROUP);
+  return getPageGroup(LIMIT_PER_PAGE_GROUP, totalPageNumber, pageGroupNumber);
+};
+
+export const changePageGroup = (
+  type: "previous" | "next",
+  limitPerPageGroup: number, // 한 번에 몇 번 페이지까지 보여줄 지
+  totalPageNumber: number, // 전체 페이지 개수
+  currentPageNumber: number = 1 // 현재 페이지 번호
+) => {
+  // 현재 페이지 그룹이 몇 번째 그룹인지
+  const currentPageGroupNumber = Math.ceil(currentPageNumber / limitPerPageGroup);
+  let nextPageGroupNumber;
+  switch (type) {
+    case "previous":
+      nextPageGroupNumber = currentPageGroupNumber - 1;
+      break;
+    case "next":
+      nextPageGroupNumber = currentPageGroupNumber + 1;
+      break;
+  }
+  return getPageGroup(limitPerPageGroup, totalPageNumber, nextPageGroupNumber);
+};
