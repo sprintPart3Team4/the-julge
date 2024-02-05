fa3877069598b3c6ef6e2bf08eed6fee131930a6
youngeun <00eun1128@gmail.com>
2024-02-05 14:49:34 +0900

    feat : 프로필에 api 연동

--- components/user/myProfile/MyProfile.tsx
+++ components/user/myProfile/MyProfile.tsx
@@ -3,29 +3,37 @@ import Image from "next/image";
 import Button from "@/components/common/button/Button";
 import { userInfo } from "@/pages/api/userMockdata";
 import LocationIcon from "@/public/images/location.svg";
-import phone from "@/public/images/phone.svg";
+import phoneIco from "@/public/images/phone.svg";
 import styles from "./MyProfile.module.scss";
+import { useAuth } from "@/contexts/AuthProvider";
 
 const cn = classNames.bind(styles);
 
 export default function MyProfile() {
+  const { user } = useAuth();
+  console.log(`유저정보 : ${user}`);
+  console.log(user);
+
+  if (!user) return;
+  const { address, bio, phone, name } = user;
+
   return (
     <div className={cn("wrap")}>
       <div className={cn("contents")}>
         <div className={cn("userInfo")}>
           <span className={cn("title")}>이름</span>
-          <span className={cn("name")}>{userInfo.item.name}</span>
+          <span className={cn("name")}>{name}</span>
           <div className={cn("location")}>
-            <Image src={phone} alt="폰 아이콘" width={20} height={20} />
-            <span>{userInfo.item.phone}</span>
+            <Image src={phoneIco} alt="폰 아이콘" width={20} height={20} />
+            <span>{phone}</span>
           </div>
           <div className={cn("location")}>
             <Image src={LocationIcon} alt="위치 아이콘" width={20} height={20} />
-            <span>{userInfo.item.address}</span>
+            <span>{address}</span>
           </div>
         </div>
         <div className={cn("userDes")}>
-          <span className={cn("description")}>{userInfo.item.bio}</span>
+          <span className={cn("description")}>{bio}</span>
         </div>
       </div>
       <div className={cn("buttons")}>
