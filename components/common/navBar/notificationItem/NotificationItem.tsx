2cf919e5a1c4470e37ea90a4a83c46a671896ec2
Jisun Park <jisunpark.t@gmail.com>
2024-02-08 02:11:11 +0900

    Feat: 알림 생성 후 경과 시간 보여주는 함수 구현

--- components/common/navBar/notificationItem/NotificationItem.tsx
+++ components/common/navBar/notificationItem/NotificationItem.tsx
@@ -5,6 +5,7 @@ import { getFullDate } from "@/lib/getFullDate";
 import ApprovedIcon from "@/public/images/circle_approved.svg";
 import RejectedIcon from "@/public/images/circle_rejected.svg";
 import styles from "./NotificationItem.module.scss";
+import { create } from "domain";
 
 const cn = classNames.bind(styles);
 
@@ -17,6 +18,25 @@ type Props = {
 };
 
 export default function NotificationItem({ name, workhour, startsAt, result, createdAt }: Props) {
+  const calculateTimeAgo = (createdAt: string) => {
+    const now = new Date();
+    const createdTime = new Date(createdAt);
+
+    const seconds = Math.floor((now.getTime() - createdTime.getTime()) / 1000);
+    if (seconds < 60) return "방금 전";
+
+    const minutes = seconds / 60;
+    if (minutes < 60) return `${Math.floor(minutes)}분 전`;
+
+    const hours = minutes / 60;
+    if (hours < 24) return `${Math.floor(hours)}시간 전`;
+
+    const days = hours / 24;
+    if (days < 7) return `${Math.floor(days)}일 전`;
+
+    return `${createdTime.toLocaleDateString()}`;
+  };
+
   return (
     <div className={cn("card")}>
       <Image src={result === "accepted" ? ApprovedIcon : RejectedIcon} alt="결과 아이콘" width={5} height={5} />
@@ -25,7 +45,7 @@ export default function NotificationItem({ name, workhour, startsAt, result, cre
         <span className={cn(result)}>{result === "accepted" ? `승인` : `거절`}</span>
         {`되었어요.`}
       </span>
-      <span className={cn("timeAgo")}>{createdAt}분 전</span> {/* TODO 몇 분 전 형식 바꿔주기 */}
+      <span className={cn("timeAgo")}>{calculateTimeAgo(createdAt)}</span>
     </div>
   );
 }
