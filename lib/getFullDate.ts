1a853bc5a4b63027ebaf493603c273cebc422663
Jisun Park <jisunpark.t@gmail.com>
2024-02-08 02:03:38 +0900

    Refactor: 알림창에서도 getFullDate를 사용할 수 있도록 옵셔널 파라미터 추가

--- lib/getFullDate.ts
+++ lib/getFullDate.ts
@@ -6,19 +6,21 @@ function getDate(date: Date) {
   return `${year}-${month}-${day}`;
 }
 
-function getTime(date: Date, workhour: number) {
+function getTime(date: Date, workhour: number, isAlert: boolean = false) {
   const hour = date.getHours();
   const endHour = hour + workhour > 24 ? hour + workhour - 24 : hour + workhour;
   const minute = (date.getMinutes() < 10 ? "0" : "") + date.getMinutes();
 
   const startTime = `${hour}:${minute}`;
   const endTime = `${endHour}:${minute}`;
-
+  if (isAlert) {
+    return `${startTime}~${endTime}`;
+  }
   return `${startTime}~${endTime} (${workhour}시간)`;
 }
 
-export function getFullDate(sourceDate: string, workhour: number) {
+export function getFullDate(sourceDate: string, workhour: number, isAlert: boolean = false) {
   const date = new Date(sourceDate);
 
-  return `${getDate(date)} ${getTime(date, workhour)}`;
+  return `${getDate(date)} ${getTime(date, workhour, isAlert)}`;
 }
