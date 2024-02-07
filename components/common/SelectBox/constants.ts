37aada537c703b8ac6616c8b2480ff6de0559617
youngeun <00eun1128@gmail.com>
2024-02-07 15:19:26 +0900

    chore : 파일 경로 이동

---
+++ components/common/SelectBox/constants.ts
@@ -0,0 +1,102 @@
+export const ADDRESS = [
+  {
+    id: "jongnoGu",
+    name: "서울시 종로구",
+  },
+  {
+    id: "jungGu",
+    name: "서울시 중구",
+  },
+  {
+    id: "yongsanGu",
+    name: "서울시 용산구",
+  },
+  {
+    id: "seongdongGu",
+    name: "서울시 성동구",
+  },
+  {
+    id: "gwangjinGu",
+    name: "서울시 광진구",
+  },
+  {
+    id: "dongdaemunGu",
+    name: "서울시 동대문구",
+  },
+  {
+    id: "jungnangGu",
+    name: "서울시 중랑구",
+  },
+  {
+    id: "seongbukGu",
+    name: "서울시 성북구",
+  },
+  {
+    id: "gangbukGu",
+    name: "서울시 강북구",
+  },
+  {
+    id: "dobongGu",
+    name: "서울시 도봉구",
+  },
+  {
+    id: "nowonGu",
+    name: "서울시 노원구",
+  },
+  {
+    id: "eunpyeongGu",
+    name: "서울시 은평구",
+  },
+  {
+    id: "seodaemunGu",
+    name: "서울시 서대문구",
+  },
+  {
+    id: "mapoGu",
+    name: "서울시 마포구",
+  },
+  {
+    id: "yangcheonGu",
+    name: "서울시 양천구",
+  },
+  {
+    id: "gangseoGu",
+    name: "서울시 강서구",
+  },
+  {
+    id: "guroGu",
+    name: "서울시 구로구",
+  },
+  {
+    id: "geumcheonGu",
+    name: "서울시 금천구",
+  },
+  {
+    id: "yeongdeungpoGu",
+    name: "서울시 영등포구",
+  },
+  {
+    id: "dongjakGu",
+    name: "서울시 동작구",
+  },
+  {
+    id: "gwanakGu",
+    name: "서울시 관악구",
+  },
+  {
+    id: "seochoGu",
+    name: "서울시 서초구",
+  },
+  {
+    id: "gangnamGu",
+    name: "서울시 강남구",
+  },
+  {
+    id: "songpaGu",
+    name: "서울시 송파구",
+  },
+  {
+    id: "gangdongGu",
+    name: "서울시 강동구",
+  },
+];
