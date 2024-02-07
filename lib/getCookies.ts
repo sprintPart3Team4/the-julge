53bfb62941fdc770f36a73de69b608b9d875cad2
Chyyni <catharina0409@gmail.com>
2024-02-07 14:13:43 +0900

    Feat: req에서 쿠키 받아오는 함수 추가

--- lib/getCookies.ts
+++ lib/getCookies.ts
@@ -2,3 +2,8 @@ export default function getCookies() {
   const cookies = Object.fromEntries(document.cookie.split(";").map((cookie) => cookie.trim().split("=")));
   return cookies;
 }
+
+export function getReqCookies(reqCookies: string) {
+  const cookies = Object.fromEntries(reqCookies.split(";").map((cookie) => cookie.trim().split("=")));
+  return cookies;
+}
