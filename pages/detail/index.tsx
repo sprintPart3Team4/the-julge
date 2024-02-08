44eccb7c39ad5bd671542da69d7cbd2a8885cf27
mijin0928 <94034865+mijin0928@users.noreply.github.com>
2024-02-08 19:06:15 +0900

    Feat: shopId, noticeId 쿼리로 받아오기

--- pages/detail/index.tsx
+++ pages/detail/index.tsx
@@ -11,7 +11,6 @@ import Pay from "@/components/shopNoticePage/pay/Pay";
 import { useAuth } from "@/contexts/AuthProvider";
 import Button from "@/components/common/button/Button";
 import Modal from "@/components/common/modal/Modal";
-import instance from "@/lib/axiosInstance";
 import getCookies from "@/lib/getCookies";
 import classNames from "classnames/bind";
 import styles from "@/styles/detail.module.scss";
@@ -79,15 +78,15 @@ export default function DetailPage() {
   const buttonColor = isFinished ? "secondary" : "primary";
   let isPast: boolean;
 
-  cardList.map((card) => {
-    const registeredDate = new Date(card.item.startsAt);
-    const today = new Date();
-    const diff = +today - +registeredDate;
+  // cardList.map((card) => {
+  //   const registeredDate = new Date(card.item.startsAt);
+  //   const today = new Date();
+  //   const diff = +today - +registeredDate;
 
-    const resultDate = Math.floor(+diff / (1000 * 60 * 60 * 24) + 1);
+  //   const resultDate = Math.floor(+diff / (1000 * 60 * 60 * 24) + 1);
 
-    isPast = resultDate > 0 ? true : false;
-  });
+  //   isPast = resultDate > 0 ? true : false;
+  // });
 
   const status = {
     status: "canceled",
@@ -142,125 +141,123 @@ export default function DetailPage() {
   };
 
   const handleLoadNoticeDetail = async () => {
-    const { token } = getCookies();
-
-    const res = await instance.get(`shops/${s}/notices/${u}`, {
-      headers: { Authorization: `Bearer ${token}` },
-    });
+    const res = await instance.get(`shops/${s}/notices/${u}`);
     setNoticeInfo(res.data.item);
     setShopInfo(res.data.item.shop.item);
   };
 
   const handleLoadNotice = async () => {
-    const res = await instance.get(`shops/${s}/notices`);
+    const res = await instance.get('notices');
     setCardList(res.data.items);
   };
 
   useEffect(() => {
-    if (s) {
+    if (u) {
       const stored = localStorage.getItem("watched");
       let watched = stored ? JSON.parse(stored) : [];
-      watched.unshift(s);
-      
+      watched.unshift(u);
+
       const uniqueWatched = watched.filter((item: string, index: number) => watched.indexOf(item) === index);
       localStorage.setItem("watched", JSON.stringify(uniqueWatched));
       setWatchedItem(cardList.filter((card) => uniqueWatched.includes(card.item.id)).slice(0, 6));
     }
-  }, [s, cardList]);
+  }, [u, cardList]);
 
   useEffect(() => {
     handleLoadNotice();
     handleLoadNoticeDetail();
   }, []);
 
+  console.log(watchedItem)
+
   return (
     <>
       <NavBar />
-    <div className={cn("detailContainer")}>
-      <div className={cn("noticeTitle")}>
-        <div>
-          <Title>
-            <Title.SubTitle subTitle={shopInfo.category} />
-            <Title.MainTitle mainTitle={shopInfo.name} />
-          </Title>
-        </div>
-        <div className={cn("panelContainer")}>
-          <Panel>
-            <div className={cn("imgWrap", { active: isClosed })}>
-              <Panel.Thumbnail src={shopInfo.imageUrl} alt={shopInfo.imageUrl} />
-              <span>마감 완료</span>
-            </div>
-            <div className={cn("contentContainer")}>
-              <div className={cn("content")}>
-                <Pay>
-                  <Pay.SubTitle subTitle="시급" />
-                  <div className={cn("payContainer")}>
-                    <Pay.HourlyPay hourlypay={noticeInfo.hourlyPay} />
-                    <Pay.HighPayRateBadge
-                      hourlyPay={noticeInfo.hourlyPay}
-                      originalHourlyPay={shopInfo.originalHourlyPay}
-                    />
-                  </div>
-                </Pay>
-                <Panel.WorkHour startsAt={noticeInfo.startsAt} workHour={noticeInfo.workhour} isClosed={false} />
-                <Panel.Address address={shopInfo.address1} isClosed={false} />
-                <Panel.shopDescription description={shopInfo.description} />
+      <div className={cn("detailContainer")}>
+        <div className={cn("noticeTitle")}>
+          <div>
+            <Title>
+              <Title.SubTitle subTitle={shopInfo.category} />
+              <Title.MainTitle mainTitle={shopInfo.name} />
+            </Title>
+          </div>
+          <div className={cn("panelContainer")}>
+            <Panel>
+              <div className={cn("imgWrap", { active: isClosed })}>
+                <Panel.Thumbnail src={shopInfo.imageUrl} alt={shopInfo.imageUrl} />
+                <span>마감 완료</span>
               </div>
-              {noticeInfo.closed ? (
-                <Button text="신청 불가" size="flexible" color="disabled" />
-              ) : (
-                <Button text={buttonType} size="flexible" color={buttonColor} handleButtonClick={handleButtonClick} />
-              )}
-            </div>
-          </Panel>
+              <div className={cn("contentContainer")}>
+                <div className={cn("content")}>
+                  <Pay>
+                    <Pay.SubTitle subTitle="시급" />
+                    <div className={cn("payContainer")}>
+                      <Pay.HourlyPay hourlypay={noticeInfo.hourlyPay} />
+                      <Pay.HighPayRateBadge
+                        hourlyPay={noticeInfo.hourlyPay}
+                        originalHourlyPay={shopInfo.originalHourlyPay}
+                      />
+                    </div>
+                  </Pay>
+                  <Panel.WorkHour startsAt={noticeInfo.startsAt} workHour={noticeInfo.workhour} isClosed={false} />
+                  <Panel.Address address={shopInfo.address1} isClosed={false} />
+                  <Panel.shopDescription description={shopInfo.description} />
+                </div>
+                {noticeInfo.closed ? (
+                  <Button text="신청 불가" size="flexible" color="disabled" />
+                ) : (
+                  <Button text={buttonType} size="flexible" color={buttonColor} handleButtonClick={handleButtonClick} />
+                )}
+              </div>
+            </Panel>
+          </div>
+          <div className={cn("desWrap")}>
+            <NoticeDescription noticeDescription={noticeInfo.description} />
+          </div>
         </div>
-        <div className={cn("desWrap")}>
-          <NoticeDescription noticeDescription={noticeInfo.description} />
-        </div>
-      </div>
-      {isModalOpen && (
-        <Modal>
-          <Modal.WarningConfirm size="small" text="내 프로필을 먼저 등록해 주세요." setIsModalOpen={setIsModalOpen} />
-        </Modal>
-      )}
-      {isFinished && isModalOpen && (
-        <Modal>
-          <Modal.YesOrNo
-            text="신청을 취소하시겠어요?"
-            yesButtonText="취소하기"
-            handleYesButtonClick={handleCancelButtonClick}
-            setIsModalOpen={setIsModalOpen}
-          />
-        </Modal>
-      )}
-      {isUser === undefined && isModalOpen && (
-        <Modal>
-          <Modal.Warning text="먼저 로그인을 해주세요" size="small" setIsModalOpen={setIsModalOpen} />
-        </Modal>
-      )}
-      {userType === "employer" && isModalOpen && (
-        <Modal>
-          <Modal.Warning text="사장님은 신청할 수 없습니다" size="small" setIsModalOpen={setIsModalOpen} />
-        </Modal>
-      )}
-      <Title>
-        <MainTitle mainTitle="최근에 본 공고" />
-      </Title>
-      <div className={cn("wrap")}>
-        {watchedItem.map((card) => {
-          return (
-            <NoticeCard
-              key={card.item.id}
-              startsAt={card.item.startsAt}
-              workhour={card.item.workhour}
-              hourlyPay={card.item.hourlyPay}
-              closed={noticeInfo.closed ? true : false}
-              isPast={isPast}
+        {isModalOpen && (
+          <Modal>
+            <Modal.WarningConfirm size="small" text="내 프로필을 먼저 등록해 주세요." setIsModalOpen={setIsModalOpen} />
+          </Modal>
+        )}
+        {isFinished && isModalOpen && (
+          <Modal>
+            <Modal.YesOrNo
+              text="신청을 취소하시겠어요?"
+              yesButtonText="취소하기"
+              handleYesButtonClick={handleCancelButtonClick}
+              setIsModalOpen={setIsModalOpen}
             />
-          );
-        })}
+          </Modal>
+        )}
+        {isUser === undefined && isModalOpen && (
+          <Modal>
+            <Modal.Warning text="먼저 로그인을 해주세요" size="small" setIsModalOpen={setIsModalOpen} />
+          </Modal>
+        )}
+        {userType === "employer" && isModalOpen && (
+          <Modal>
+            <Modal.Warning text="사장님은 신청할 수 없습니다" size="small" setIsModalOpen={setIsModalOpen} />
+          </Modal>
+        )}
+        <Title>
+          <MainTitle mainTitle="최근에 본 공고" />
+        </Title>
+        <div className={cn("wrap")}>
+          {watchedItem.map((card) => {
+            return (
+              <NoticeCard
+                key={card.item.id}
+                startsAt={card.item.startsAt}
+                workhour={card.item.workhour}
+                hourlyPay={card.item.hourlyPay}
+                closed={noticeInfo.closed ? true : false}
+                isPast={isPast}
+              />
+            );
+          })}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
-
