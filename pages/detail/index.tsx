e66dde3985a8586b2f597d8752643fd0169c62c6
mijin0928 <94034865+mijin0928@users.noreply.github.com>
2024-02-12 21:06:18 +0900

    Feat: 카드 컴포넌트에 지난 공고 로직 추가

--- pages/detail/index.tsx
+++ pages/detail/index.tsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import instance from "@/lib/axiosInstance";
-import NavBar from "@/components/common/navBar/NavBar";
 import NoticeCard from "@/components/noticeList/noticeCard/noticeCard";
 import MainTitle from "@/components/common/titleBox/mainTitle/MainTitle";
 import Title from "@/components/common/titleBox/title/Title";
@@ -78,7 +77,6 @@ export default function DetailPage() {
   const isClosed = noticeInfo.closed ? "active" : "";
   const buttonType = isFinished ? "취소하기" : "신청하기";
   const buttonColor = isFinished ? "secondary" : "primary";
-  let isPast: boolean;
 
   const status = {
     status: "canceled",
@@ -137,17 +135,6 @@ export default function DetailPage() {
     const res = await instance.get("notices?limit=100");
     setCardList(res.data.items);
   };
-
-  
-  cardList.map((card) => {
-    const registeredDate = new Date(card.item.startsAt);
-    const today = new Date();
-    const diff = +today - +registeredDate;
-
-    const resultDate = Math.floor(+diff / (1000 * 60 * 60 * 24) + 1);
-
-    isPast = resultDate > 0 ? true : false;
-  });
 
   useEffect(() => {
     if (u) {
@@ -250,7 +237,6 @@ export default function DetailPage() {
                 workhour={card.item.workhour}
                 hourlyPay={card.item.hourlyPay}
                 closed={noticeInfo.closed ? true : false}
-                isPast={isPast}
                 noticeId={card.item.id}
                 noticeShopId={card.item.shop.id}
                 imageUrl={card.item.shop.item.imageUrl}
