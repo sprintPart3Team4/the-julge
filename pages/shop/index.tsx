6933c25e267c34d13202d767f491cc9eb9de62b8
Jisun Park <jisunpark.t@gmail.com>
2024-02-12 19:47:54 +0900

    Fix: 사장님/ 공고 유무에 따라 공고 카드 나타나도록

--- pages/shop/index.tsx
+++ pages/shop/index.tsx
@@ -1,10 +1,14 @@
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 import Footer from "@/components/common/footer/Footer";
 import NavBar from "@/components/common/navBar/NavBar";
 import ShopPageLayout from "@/components/shop/shopPageLayout/ShopPageLayout";
 import { useAuth } from "@/contexts/AuthProvider";
+import instance from "@/lib/axiosInstance";
 
 export default function ShopPage() {
+  const [hasNotice, setHasNotice] = useState<boolean>(false);
+
   const router = useRouter();
 
   const handleEditClick = () => {
@@ -17,7 +21,18 @@ export default function ShopPage() {
 
   const { user, shop } = useAuth();
   const hasShop = Boolean(user?.shop);
-  let hasNotice = Boolean(shop?.id);
+
+  const checkHasShop = async (shopId: string | undefined) => {
+    const res = await instance.get(`shops/${shopId}/notices`);
+    const result = res.data.items.length ? true : false;
+    setHasNotice(result);
+  };
+
+  useEffect(() => {
+    if (shop) {
+      checkHasShop(shop.id);
+    }
+  }, []);
 
   return (
     <>
