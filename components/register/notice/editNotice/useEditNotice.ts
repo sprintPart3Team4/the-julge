d1ec10ab2ccd320f126c2b71140bcf4bb7d0644a
Hyunsoo_kim <red_panda119@naver.com>
2024-02-12 15:48:18 +0900

    Style: 사장님/공고편집 console코드 삭제

--- components/register/notice/editNotice/useEditNotice.ts
+++ components/register/notice/editNotice/useEditNotice.ts
@@ -37,12 +37,10 @@ export default async function useEditNotice(
     const res = await instance.put(`shops/${shopId}/notices/${noticeId}`, requestBody, {
       headers: { Authorization: `Bearer ${token}` },
     });
-    console.log("API 수정 완료");
     console.log(res.data);
     setModal((prevState: ModalType) => ({ ...prevState, modalText: "수정되었습니다" }));
     setModal((prevState: ModalType) => ({ ...prevState, editSuccessModal: true }));
   } catch (error) {
-    console.error("API 수정 중 오류 발생", error);
     setModal((prevState: ModalType) => ({ ...prevState, modalText: "수정에 실패했습니다" }));
     setModal((prevState: ModalType) => ({ ...prevState, editFailModal: true }));
   }
